e5e61a9cc00aed9b8e3a8b0343b6c6cdd64e03ac
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-03-17 17:17:29 -0300

    feat: add product and customer factories

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,6 +1,6 @@
 import { ProductInterface } from './product.interface'
 
-export class Product implements ProductInterface {
+export class ProductB implements ProductInterface {
   private _id: string
   private _name: string
   private _price: number
@@ -31,7 +31,7 @@ export class Product implements ProductInterface {
   }
 
   get price(): number {
-    return this._price
+    return this._price * 2
   }
 
   private validate() {
